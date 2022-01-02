8723e1949aa4ed5b5e654270d000b56db801ccf1
root <root@beer.localdomain>
2022-01-02 15:57:32 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');  
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static('client'));
+
+io.on('connection', socket => {
+    console.log("New connection!");
+
+    socket.emit('message', 'Hello!');
+
+    socket.broadcast.emit('message', 'Anyone there?');
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'Goodbye!');
+    });
+
+    socket.on('chatMessage', msg =>{
+        io.emit('message', msg);
+    })
+})
+
+server.listen(port, () => {
+    console.log(`App now listening at http://localhost:${port}`);
+});
+
